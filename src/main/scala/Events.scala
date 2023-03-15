91befa7c8f83b1afd5c1f440818527858e3bc82d
allan <allan.kiironen@aalto.fi>
2023-03-15 14:53:50 +0200

    Implement read file

--- src/main/scala/Events.scala
+++ src/main/scala/Events.scala
@@ -1,23 +1,18 @@
-import java.io.{
-  FileReader,
-  FileNotFoundException,
-  BufferedReader,
-  IOException,
-  FileWriter,
-  BufferedWriter
-}
-import scala.util.Random.*
+import java.io.{BufferedReader, BufferedWriter, FileNotFoundException, FileReader, FileWriter, IOException}
 import java.util.UUID
+import scala.collection.mutable.Map
+
 
 class Events(userInput: String) {
 
-  // Writes user inputs to a txt file.
+  // Writes user inputs to a txt file in a correct format.
 
   def writetoFile: Unit =
-    val fileIn = FileWriter("eventInfo.txt")
+    val fileIn = FileWriter("eventInfo.txt",true) // set to true so the writing will append to a file
     var lineWriter = BufferedWriter(fileIn)
 
     try
+      lineWriter.newLine()
       for i <- iCalendarFormat.indices do
         lineWriter.write(iCalendarFormat(i))
         lineWriter.newLine()
@@ -28,7 +23,7 @@ class Events(userInput: String) {
     finally
       lineWriter.close()
       fileIn.close()
-  //icsFOrmatdontmodify is in ICS format. The program is not going to modify these indexes.
+
   //resultVcalendar is in ICS format. these indexes will be modified by program.
 
   def iCalendarFormat: Seq[String] =
@@ -37,6 +32,50 @@ class Events(userInput: String) {
     var resultVcalendar = Seq[String]("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//My Calendar Program//Example Corp//EN\nCALSCALE:GREGORIAN\n\nBEGIN:VEVENT" ,"UID:" + UUID.randomUUID() + "-1234567890@example.com", "DTSTAMP:" + userInputSplitted(0)
       , "DTSTART:" + userInputSplitted(1) + "z", "DTEND:" + userInputSplitted(2) + "z", "SUMMARY:" + userInputSplitted(3), "END:VEVENT\n\nEND:VCALENDAR")
     resultVcalendar
+
+
+  // Reads the whole file and stores the data into a Map.
+  // This map can be used to process the data elsewhere.
+  // UID is used as a key, to be changed later
+
+  def readFile: Map[String,Seq[String]] =
+    var storedEvents = Map[String,Seq[String]]()
+    var mapKey: String = ""
+    val lineReader = try BufferedReader(FileReader("eventInfo.txt"))
+    
+      catch
+        case e: FileNotFoundException => return storedEvents
+        case e: IOException => return storedEvents
+
+    var oneline: String = lineReader.readLine()
+    while oneline != null do
+    
+      if oneline.contains("UID:") then
+        mapKey = oneline.drop(4).take(36)
+        var mapData =  Seq[String]()
+        oneline = lineReader.readLine()
+        for i <- 0 until(4) do
+          if i == 0 then
+            mapData = mapData :+ oneline.drop(8)
+          if i == 1 then
+            mapData = mapData :+ oneline.drop(8)
+          if i == 2 then
+            mapData = mapData :+ oneline.drop(6)
+          if i == 3 then
+            mapData = mapData :+ oneline.drop(8)
+
+          oneline = lineReader.readLine()
+        storedEvents = storedEvents + (mapKey->mapData)
+      oneline = lineReader.readLine()
+    storedEvents
+
+  def addEvent = writetoFile
+  
+  def editEvent = ???
+  
+  def deleteEvent = ???
+
+
 
 
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,27 @@
 import scala.io.StdIn.*
 
 object Main extends App {
-  println("Write the event name, date, starTime, endTime and description. Seperate these by comma ','")
-  //Should work with this input = name,202003031600,202003031700, hopefully this works :D
+    //Should work with this input = name,202003031600,202003031700, hopefully this works :D
+
+  println("Choose to add an event, edit event or delete an event")
   var userInput = readLine()
-  Events(userInput).writetoFile
+  if userInput.toLowerCase == "add" then
+    println("Write the event name, date, starTime, endTime and description. Seperate these by comma ','")
+    userInput = readLine()
+
+  if userInput.toLowerCase == "edit" then
+    println("Which event do you wish to edit\nList of recorded events: " + Events(userInput).readFile.keys)
+    userInput = readLine()
+
+  if userInput.toLowerCase == "delete" then
+    println("Which event do you wish to delete\nList of recorded events: " + Events(userInput).readFile.keys)
+    userInput = readLine()
+
+
+
+
+
+
 
 
 
